681c7c882e5fb11fe1ab0629290ee2d25b4b1a6d
jennikins813 <jennikins813@yahoo.com>
2014-03-25 15:30:18 -0400

    ruby exercise 2

---
+++ exercise2.rb
@@ -0,0 +1,12 @@
+puts 55 * 0.10 # tip 10percent on 55dollar meal
+
+'puts'
+
+puts "10" + 20.to_s #convert 20 to a string to add two strings
+
+'puts'
+
+num1 = 45628
+num2 = 7839
+
+puts "#{num1 * num2}" #multiplies the values of num1 and num2
